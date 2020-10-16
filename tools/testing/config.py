4930fd47b59354ec2dfe0601043e467f6f5960da
Ian Koide <ian.koide@protonmail.com>
2020-10-16 22:20:16 +0000

    remove testing

--- tools/testing/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-  SECRET_KEY = "KXPeA6Wb5rHEGb6Dzxsxkvv6MKdMs5e2DESQArRwFqLXstsB"
-  SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "project/site.db")
